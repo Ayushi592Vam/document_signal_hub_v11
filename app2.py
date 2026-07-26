8c3d213e6049b9db2f142e32d167196bdf05bc09
Ayushi Nagpure <ayushi.nagpure@valuemomentum.com>
2026-07-26 21:22:36 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1035,7 +1035,7 @@ else:
         st.stop()
 
     curr_claim = data[st.session_state.selected_idx]
-    st.write("DEBUG curr_claim:", curr_claim)
+   
     
     _frozen_id_key = f"_frozen_claim_id_{selected_sheet}_{st.session_state.selected_idx}"
     if _frozen_id_key not in st.session_state:
